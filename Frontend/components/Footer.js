243b41e4c9d3cd313ae0f607c3f6043881188abd
fromjyce <jaya2004kra@gmail.com>
2025-01-16 14:59:44 +0530

    added footer

---
+++ Frontend/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => (
+    <footer className="bg-gray-900 text-white py-2">
+    <div className="container mx-auto text-center">
+      <p className="text-sm">Developed with ❤️ by <strong>Team MetaTrace</strong>.</p>
+    </div>
+  </footer>
+  );
+  
+  export default Footer;
+  
