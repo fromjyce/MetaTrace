936081549ae581c776ea4fd582f6964b6ae0ed8c
fromjyce <jaya2004kra@gmail.com>
2025-01-17 16:06:39 +0530

    added font change

--- Frontend/components/Footer.js
+++ Frontend/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
     <footer className="bg-[#1c1c1cff] text-[#f7f7f7ff] py-2">
     <div className="container mx-auto text-center">
-      <p className="text-sm poppins">Developed with ❤️ by <strong className="text-[#ef4d31ff]">Team MetaTrace</strong>.</p>
+      <p className="text-sm epilogue">Developed with ❤️ by <strong className="text-[#ef4d31ff] poppins">Team MetaTrace</strong>.</p>
     </div>
   </footer>
   );
