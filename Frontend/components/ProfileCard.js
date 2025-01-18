88c71fca8feb31bbf63d21b29a630da135c53d47
fromjyce <jaya2004kra@gmail.com>
2025-01-18 12:56:14 +0530

    added precursor version

--- Frontend/components/ProfileCard.js
+++ Frontend/components/ProfileCard.js
@@ -1,21 +1,21 @@
 export default function ProfileCard({ name, email, password }) {
     return (
-      <div className="bg-[#EDEADE] rounded-xl shadow-md p-6">
-        <div className="space-y-2">
+      <div className="bg-[#fefefa] rounded-xl shadow-md p-6">
+        <div className="space-y-4 poppins">
           <div className="flex items-center justify-between">
-            <span className="text-gray-600">Name:</span>
-            <span className="text-gray-800 font-medium">{name}</span>
+                <span className="text-[#5e5e5eff] font-semibold text-xl">Name</span>
+            <span className="text-[#1c1c1cff] text-xl font-medium">{name}</span>
           </div>
           <div className="flex items-center justify-between">
-            <span className="text-gray-600">Email:</span>
-            <span className="text-gray-800 font-medium">{email}</span>
+            <span className="text-[#5e5e5eff] font-semibold text-xl">Email</span>
+            <span className="text-[#1c1c1cff] text-xl font-medium">{email}</span>
           </div>
           <div className="flex items-center justify-between">
-            <span className="text-gray-600">Password:</span>
-            <span className="text-gray-800 font-medium">{'*'.repeat(password.length)}</span>
+            <span className="text-[#5e5e5eff] font-semibold text-xl">Password</span>
+            <span className="text-[#1c1c1cff] text-xl font-medium">{'*'.repeat(password.length)}</span>
           </div>
         </div>
-        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">
+        <button className="mt-4 bg-[#ef4d31ff] text-[#f7f7f7ff] px-4 py-2 rounded-lg shadow font-semibold hover:bg-[#bf3e27] poppins">
           Change Password
         </button>
       </div>
