1900f9b820057dbcb60771003f5a858fa2650c47
fromjyce <jaya2004kra@gmail.com>
2025-02-04 19:04:33 +0530

    added api for signup

---
+++ Frontend/pages/api/signup.js
@@ -0,0 +1,33 @@
+import { connectToDatabase } from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).json({ error: "Email already exists." });
+    }      
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
