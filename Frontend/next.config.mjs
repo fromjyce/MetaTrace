fb13a9fce98a844d388006ba3c896fac3f28b1e5
fromjyce <jaya2004kra@gmail.com>
2025-03-11 11:47:02 +0530

    added

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          net: false,
+          tls: false,
+          fs: false,
+          "child_process": false,
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
