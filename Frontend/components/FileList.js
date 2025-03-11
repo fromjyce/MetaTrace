bc1c58ead3726683ca662e0b1eff0b9177e1d367
fromjyce <jaya2004kra@gmail.com>
2025-03-11 10:38:50 +0530

    added all the necessary buttons

--- Frontend/components/FileList.js
+++ Frontend/components/FileList.js
@@ -1,4 +1,5 @@
-import { Info, Trash2, Download, FileImage, FileText, FileVideo, FileMusic, FileArchive, File } from 'lucide-react';
+import { Info, Trash2, Download, FileImage, FileText, FileVideo, FileMusic, FileArchive, File, Upload } from 'lucide-react';
+import { useRouter } from 'next/router';
 
 const getFileIcon = (fileType) => {
   if (fileType.startsWith('image')) {
@@ -29,7 +30,8 @@ const handleDownload = (upload) => {
   window.URL.revokeObjectURL(url);
 };
 
-export default function FileList({ files, onDelete, onMetadataClick }) {
+export default function FileList({ files, onDelete, onMetadataClick, onAllDelete }) {
+  const router = useRouter();
   const handleDelete = async (upload) => {
     if (!onDelete) {
       console.error("onDelete function is not defined.");
@@ -54,55 +56,128 @@ export default function FileList({ files, onDelete, onMetadataClick }) {
     }
   };
 
+  const handleDownloadAll = () => {
+    const dataStr = JSON.stringify(files, null, 2);
+    const dataBlob = new Blob([dataStr], { type: 'application/json' });
+    const url = window.URL.createObjectURL(dataBlob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `all_files.json`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  };
+
+  const handleDeleteAll = async () => {
+    const confirmDelete = window.confirm("Are you sure you want to delete all files?");
+    if (confirmDelete) {
+      try {
+        const response = await fetch('/api/deleteAllFiles', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ fileIds: files.map((file) => file._id) }),
+        });
+
+        if (response.ok) {
+          onAllDelete(null); // Clear the files state
+        } else {
+          console.error("Failed to delete all files");
+        }
+      } catch (error) {
+        console.error("Error deleting all files:", error);
+      }
+    }
+  };
+
+
+  const handleUpload = () => {
+    router.push('/upload');
+  };
+
+
   return (
-    <div className="bg-[#fefefa] rounded-xl shadow-md p-6">
-      <table className="w-full text-left">
-        <thead>
-          <tr className="border-b">
-            <th className="py-3 px-4 text-lg font-semibold text-gray-800">File Name</th>
-            <th className="py-3 px-4 text-lg font-semibold text-gray-800">Upload Date</th>
-            <th className="py-3 px-4 text-lg font-semibold text-gray-800 text-right">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {files.map((file) => (
-            <tr key={file._id} className="border-b hover:bg-gray-50 transition-colors">
-              <td className="py-3 px-4 flex items-center">
-                <div className="mr-3">
-                  {getFileIcon(file.type)} {/* File icon on the left */}
-                </div>
-                <span className="text-gray-700 font-medium">{file.filename}</span>
-              </td>
-              <td className="py-3 px-4 text-gray-600">
-                {new Date(file.uploadDate).toLocaleDateString()}
-              </td>
-              <td className="py-3 px-4 flex items-center justify-end space-x-3">
-                <button
-                  className="p-2 bg-[#4CBB17] text-white rounded-lg hover:bg-[#2E8B57] transition-colors"
-                  aria-label="View Metadata"
-                  onClick={() => onMetadataClick(file)}
-                >
-                  <Info className="w-5 h-5" />
-                </button>
-                <button
-                  className="p-2 bg-[#FF4433] text-white rounded-lg hover:bg-[#D22B2B] transition-colors"
-                  aria-label="Delete File"
-                  onClick={() => handleDelete(file)}
-                >
-                  <Trash2 className="w-5 h-5" />
-                </button>
-                <button
-                  className="p-2 bg-[#4169E1] text-white rounded-lg hover:bg-[#0F52BA] transition-colors"
-                  aria-label="Download File"
-                  onClick={() => handleDownload(file)}
-                >
-                  <Download className="w-5 h-5" />
-                </button>
-              </td>
+    <div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-2xl font-bold text-gray-800 epilogue">
+          File <span className="text-[#ef4d31ff]">History</span>
+        </h2>
+        <div className="flex items-center space-x-3">
+          <button
+            className="p-2 bg-[#4169E1] text-white rounded-lg hover:bg-[#0F52BA] transition-colors flex items-center"
+            onClick={handleDownloadAll}
+          >
+            <Download className="w-5 h-5 mr-2" />
+            <span>Download All</span>
+          </button>
+          <button
+            className="p-2 bg-[#FF4433] text-white rounded-lg hover:bg-[#D22B2B] transition-colors flex items-center"
+            onClick={handleDeleteAll}
+          >
+            <Trash2 className="w-5 h-5 mr-2" />
+            <span>Delete All</span>
+          </button>
+          <button
+            className="p-2 bg-[#4CBB17] text-white rounded-lg hover:bg-[#2E8B57] transition-colors flex items-center"
+            onClick={handleUpload}
+          >
+            <Upload className="w-5 h-5 mr-2" />
+            <span>Upload</span>
+          </button>
+        </div>
+      </div>
+
+      <div className="bg-[#fefefa] rounded-xl shadow-md p-6">
+        <table className="w-full text-left">
+          <thead>
+            <tr className="border-b">
+              <th className="py-3 px-4 text-lg font-semibold text-gray-800">File Name</th>
+              <th className="py-3 px-4 text-lg font-semibold text-gray-800">Upload Date</th>
+              <th className="py-3 px-4 text-lg font-semibold text-gray-800 text-right">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {files.map((file) => (
+              <tr key={file._id} className="border-b hover:bg-gray-50 transition-colors">
+                <td className="py-3 px-4 flex items-center">
+                  <div className="mr-3">
+                    {getFileIcon(file.type)} {/* File icon on the left */}
+                  </div>
+                  <span className="text-gray-700 font-medium">{file.filename}</span>
+                </td>
+                <td className="py-3 px-4 text-gray-600">
+                  {new Date(file.uploadDate).toLocaleDateString()}
+                </td>
+                <td className="py-3 px-4 flex items-center justify-end space-x-3">
+                  <button
+                    className="p-2 bg-[#4CBB17] text-white rounded-lg hover:bg-[#2E8B57] transition-colors"
+                    aria-label="View Metadata"
+                    onClick={() => onMetadataClick(file)}
+                  >
+                    <Info className="w-5 h-5" />
+                  </button>
+                  <button
+                    className="p-2 bg-[#FF4433] text-white rounded-lg hover:bg-[#D22B2B] transition-colors"
+                    aria-label="Delete File"
+                    onClick={() => handleDelete(file)}
+                  >
+                    <Trash2 className="w-5 h-5" />
+                  </button>
+                  <button
+                    className="p-2 bg-[#4169E1] text-white rounded-lg hover:bg-[#0F52BA] transition-colors"
+                    aria-label="Download File"
+                    onClick={() => handleDownload(file)}
+                  >
+                    <Download className="w-5 h-5" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
