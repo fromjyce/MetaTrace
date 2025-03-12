32a570dd7931bee6ca2cd9dfc1379fcee83b982d
fromjyce <jaya2004kra@gmail.com>
2025-03-12 20:06:09 +0530

    reduce max height of MetadataRecommendations component for improved layout

--- Frontend/components/MetadataRecommendations.js
+++ Frontend/components/MetadataRecommendations.js
@@ -24,7 +24,7 @@ const MetadataAndRecommendations = ({ metadata, onBackToUpload }) => {
             <List className="w-5 h-5 mr-2 text-[#ef4d31ff]" />
             <h4 className="text-lg font-bold epilogue">Metadata</h4>
           </div>
-          <div className="overflow-y-auto max-h-96">
+          <div className="overflow-y-auto max-h-80">
             <ul className="list-none text-sm poppins">
               {metadata ? (
                 Object.entries(metadata).map(([key, value]) => (
@@ -46,7 +46,7 @@ const MetadataAndRecommendations = ({ metadata, onBackToUpload }) => {
             <BrainCog className="w-5 h-5 mr-2 text-[#ef4d31ff]" />
             <h4 className="text-lg font-bold epilogue">AI Recommendations</h4>
           </div>
-          <div className="overflow-y-auto max-h-96">
+          <div className="overflow-y-auto max-h-80">
             <p className="text-sm text-gray-700 poppins">
               AI recommendations will be displayed here.
             </p>
