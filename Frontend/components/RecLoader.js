be34b4d284475c95f28352b9cc2337690eb11040
fromjyce <jaya2004kra@gmail.com>
2025-03-12 19:18:11 +0530

    remove unnecessary styles from RecLoader component

--- Frontend/components/RecLoader.js
+++ Frontend/components/RecLoader.js
@@ -19,15 +19,6 @@ const RecLoader = () => {
 }
 
 const StyledWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-  top: 0;
-  left: 0;
-
   .dot-spinner {
     --uib-size: 3rem;
     --uib-speed: .9s;
